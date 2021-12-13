ce8f4a492d8fc663284d5cc371d2b4845982a943
Lexx YungCarter <spencermainaa@gmail.com>
2021-12-13 16:23:44 +0300

    feat: added more fields to country table seeder

--- src/Models/Country.php
+++ src/Models/Country.php
@@ -3,13 +3,20 @@
 namespace AceLords\Core\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Str;
 
 class Country extends Model
 {
     protected $table = "countries";
     
     protected $fillable = [
-        'name', 'slug', 'iso_code', 'country_code'
+        'name', 'slug', 'iso_code', 'country_code',
+        'region', 'sub_region', 'lat_lng', 'timezone_offset', 
+        'currency_code', 'currency_name', 'currency_symbol',
+    ];
+
+    protected $casts = [
+        'lat_lng' => 'array'
     ];
     
     public $timestamps = false;
@@ -28,7 +35,16 @@ class Country extends Model
     public function setNameAttribute($value)
     {
         $this->attributes['name'] = $value;
-        $this->attributes['slug'] = str_slug($value, "-");
+        $this->attributes['slug'] = Str::slug($value, "-");
+    }
+
+    /**
+     * Get the timezone 
+     * e.g. Africa/Nairobi
+     */
+    public function getTimezoneAttribute()
+    {
+        return $this->region . '/' . $this->subregion;
     }
 
 }
